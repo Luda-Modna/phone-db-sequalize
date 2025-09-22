72dbc3db28455c6082be0253fb1b9e2b7f07e428
Luda Modna <ludmilamodna3@gmail.com>
2025-09-22 19:10:02 +0300

    Add models + migrations

---
+++ migrations/20250922155445-create-phone.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Phones', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      productYear: {
+        type: Sequelize.DATE
+      },
+      sizeRAM: {
+        type: Sequelize.INTEGER
+      },
+      processor: {
+        type: Sequelize.STRING
+      },
+      screenDiagonal: {
+        type: Sequelize.INTEGER
+      },
+      isNFC: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Phones');
+  }
+};
---
+++ models/phone.js
@@ -0,0 +1,66 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Phone extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Phone.init(
+    {
+      model: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [2, 50],
+        },
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [2, 50],
+        },
+      },
+      productYear: {
+        type: DataTypes.DATEONLY,
+        validate: { isBefore: new Date().toISOString() },
+      },
+      sizeRAM: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: 1,
+          max: 128,
+          isInt: true,
+        },
+      },
+      processor: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: true,
+          len: [2, 50],
+        },
+      },
+      screenDiagonal: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: 3,
+          max: 10,
+        },
+      },
+      isNFC: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'Phone',
+    }
+  );
+  return Phone;
+};
