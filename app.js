e0fc37e2fc63926a9782f74d54e859a69fa86ccc
Luda Modna <ludmilamodna3@gmail.com>
2025-10-03 17:02:59 +0300

    Add base server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { errorHandlers } = require('./middleware');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
+
+module.exports = app;
