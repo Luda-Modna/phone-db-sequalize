dc037288405bfbdcb9f116f65e0018252e8b526b
Luda Modna <ludmilamodna3@gmail.com>
2025-10-03 17:21:11 +0300

    Add routing

--- routes/phonesRouter.js
+++ routes/phonesRouter.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
+const { phonesController } = require('../controllers');
 
 const phonesRouter = Router();
 
-phonesRouter.route('/').get().post();
+phonesRouter
+  .route('/')
+  .get(phonesController.getPhones)
+  .post(phonesController.createPhone);
 
-phonesRouter.route('/:id').get().patch().put().delete();
+phonesRouter
+  .route('/:id')
+  .get(phonesController.getPhoneById)
+  .patch(phonesController.updatePhoneById)
+  .put(phonesController.updateOrCreatePhone, phonesController.createPhone)
+  .delete(phonesController.deletePhoneById);
 
 module.exports = phonesRouter;
