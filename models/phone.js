71c3a124162983c4ba0044a5f7c922b6b19bbda3
Luda Modna <ludmilamodna3@gmail.com>
2025-10-10 16:21:02 +0300

    Add phone foreign key

--- models/phone.js
+++ models/phone.js
@@ -2,13 +2,15 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Phone extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
-      // define association here
+      Phone.hasMany(models.Preorder, {
+        foreignKey: {
+          name: 'phoneId',
+          allowNull: false,
+        },
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+      });
     }
   }
   Phone.init(
